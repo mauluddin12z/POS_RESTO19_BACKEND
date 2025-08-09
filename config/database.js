00bd3d1a29e059033f60750241670b0d419c86e0
Muhammad Hidayat Mauluddin <mauludindayat@gmail.com>
2025-08-09 23:40:03 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,12 +1,16 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import mysql2 from "mysql2";
+
 dotenv.config();
+
 // Create a new Sequelize instance using the URI
 const db = new Sequelize(process.env.MYSQL_URI, {
    dialect: "mysql",
    timezone: "+07:00",
-   dialectModule: require("mysql2"),
+   dialectModule: mysql2,  // Use the imported mysql2 module
 });
+
 
 export default db;
 
