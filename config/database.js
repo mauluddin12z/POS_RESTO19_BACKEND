339aed8453959746be49759d7c224acfbf4793f2
Muhammad Hidayat Mauluddin <mauludindayat@gmail.com>
2025-08-09 23:23:16 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
-
-const db = new Sequelize("resto_19", "root", "", {
-   host: "localhost",
+import dotenv from "dotenv";
+dotenv.config();
+// Create a new Sequelize instance using the URI
+const db = new Sequelize(process.env.MYSQL_URI, {
    dialect: "mysql",
    timezone: "+07:00",
 });
-
 
 export default db;
