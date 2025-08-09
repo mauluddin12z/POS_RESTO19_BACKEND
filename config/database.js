773d0825a0d02acc5c6cc4345ce7bb9c0cf4bb7e
Muhammad Hidayat Mauluddin <mauludindayat@gmail.com>
2025-08-09 23:32:18 +0700

    add vercel json

--- config/database.js
+++ config/database.js
@@ -1,8 +1,18 @@
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+// dotenv.config();
+// // Create a new Sequelize instance using the URI
+// const db = new Sequelize(process.env.MYSQL_URI, {
+//    dialect: "mysql",
+//    timezone: "+07:00",
+// });
+
+// export default db;
+
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-dotenv.config();
-// Create a new Sequelize instance using the URI
-const db = new Sequelize(process.env.MYSQL_URI, {
+
+const db = new Sequelize("resto_19", "root", "", {
+   host: "localhost",
    dialect: "mysql",
    timezone: "+07:00",
 });
